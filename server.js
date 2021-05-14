c754e6ad5e3091d17ca7771f93ca3defad250065
Sanday08 <sandip.job89@gmail.com>
2021-05-15 02:23:27 +0530

    add

--- server.js
+++ server.js
@@ -18,19 +18,6 @@ const hpp = require("hpp");
 
 
 
-//Set you offset here like +5.5 for IST
-var offsetIST = 19800000;
-
-//Create a new date from the Given string
-var d = new Date();
-
-//To convert to UTC datetime by subtracting the current Timezone offset
-var utcdate = new Date(d.getTime());
-
-//Then cinver the UTS date to the required time zone offset like back to 5.5 for IST
-var istdate = new Date(utcdate.getTime() + offsetIST)
-
-
 
 
 
@@ -57,7 +44,9 @@ const io = socketio(server);
 
 //file upload middleware allways add first
 app.use(fileUpload());
-console.log("Today is :",new Date());
+console.log("Today is :",new Date().toLocaleString('en-US', {
+  timeZone: 'Asia/Calcutta'
+}));
 //Body parser
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
